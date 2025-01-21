bf72692ddb5ddc38c1f87eaddb7f9e98e27e0470
Vishwanath K Khuli <152583197+vishwanathkhuli@users.noreply.github.com>
2025-01-21 23:23:34 +0530

    update backend

--- Backend/src/main/java/com/example/taskManager/config/TaskManagerSecurityConfig.java
+++ Backend/src/main/java/com/example/taskManager/config/TaskManagerSecurityConfig.java
@@ -67,7 +67,7 @@ public class TaskManagerSecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost/5173", "http://localhost:3000", "https://task-manager-fnnp1dwuj-vishwanathkhulis-projects.vercel.app/")
+                        .allowedOrigins("http://localhost/5173", "http://localhost:3000", "https://task-manager-snowy-nine.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
